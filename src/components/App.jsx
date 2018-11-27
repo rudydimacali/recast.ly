3191e3cde2448e688935c6fafc1c42053d87f4e8
Hack Reactor Student <student@hackreactor.com>
2018-11-26 21:22:16 -0800

    Completed recast.ly through searchYouTube.js.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,9 @@
 import VideoList from './VideoList.js';
 import VideoPlayer from './VideoPlayer.js';
+import Search from './Search.js';
 import exampleVideoData from '/src/data/exampleVideoData.js';
+import searchYoutube from '/src/lib/searchYoutube.js';
+import YOUTUBE_API_KEY from '/src/config/youtube.js';
 
 // var App = () => (
 //   <div>
@@ -40,7 +43,7 @@ class App extends React.Component {
     return (<div>
       <nav className="navbar">
         <div className="col-md-6 offset-md-3">
-          <div><h5><em>search</em> view goes here</h5></div>
+          <div><h5><em>search</em><Search /></h5></div>
         </div>
       </nav>
       <div className="row">
@@ -48,7 +51,7 @@ class App extends React.Component {
           <div><h5><em>videoPlayer</em><VideoPlayer video={this.state.currentVideo}/></h5></div>
         </div>
         <div className="col-md-5">
-          <div><h5><em>videoList</em><VideoList videos={exampleVideoData} clickFunc={this.onVideoListEntryClick}/></h5></div>
+          <div><h5><em>videoList</em><VideoList videos={this.state.currentVideoList} clickFunc={this.onVideoListEntryClick}/></h5></div>
         </div>
       </div>
     </div>);
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,12 @@
 var searchYouTube = (options, callback) => {
-  return;
+  $.get('https://www.googleapis.com/youtube/v3/search',
+    { 'key': options.key,
+      'part': 'id,snippet',
+      'maxResults': options.max || '5',
+      'q': options.query,
+      'type': 'video',
+      'videoEmbeddable': 'true',
+    }, (data) => {callback(data.items);});
 };
 
 export default searchYouTube;
